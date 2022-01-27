d5da0bc329baa5607354a1355b261a27fb58b131
yootaek <eldbxor@naver.com>
2022-01-27 10:47:28 +0900

    Link를 이용한 페이지 주소 변경

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Routes>
         <Route path="/" element={<Home />} exact={true} />
         <Route path="/about" element={<About />} />
