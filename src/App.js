09a6977998d083a0d5d4fd619dc28b78e22a3267
yootaek <eldbxor@naver.com>
2022-02-09 10:51:27 +0900

    유동적인 URL 파라미터 받기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Profile from "./Profile";
 
 const App = () => {
   return (
@@ -12,11 +13,18 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/yootaek">yootaek 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Routes>
         <Route path="/" element={<Home />} exact={true} />
         <Route path="/about" element={<About />} />
+        <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </div>
   );
